ad610f5aef87c21d44d3e7a8ea86b1ec67e85dab
pedro.malave <pmalavev@fpcoruna.afundacion.org>
2023-01-30 13:42:32 +0100

    se agrego lo de progress bar

--- app/src/main/java/com/afundacion/gestorfinanzas/graphline/GraphDataFragment.java
+++ app/src/main/java/com/afundacion/gestorfinanzas/graphline/GraphDataFragment.java
@@ -2,65 +2,62 @@ package com.afundacion.gestorfinanzas.graphline;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.afundacion.gestorfinanzas.R;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GraphDataFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class GraphDataFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+    private static  String URL="https://63c6654ddcdc478e15c08b47.mockapi.io";
+    private RequestQueue queue;
+
+    private ProgressBar progressBar;
 
     public GraphDataFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EstadisticasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GraphDataFragment newInstance(String param1, String param2) {
-        GraphDataFragment fragment = new GraphDataFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graph_data, container, false);
+        View view=inflater.inflate(R.layout.fragment_graph_data, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        this.queue = Volley.newRequestQueue(getContext());
+        this.progressBar=view.findViewById(R.id.progress_circular);
+
+        transacs = new ArrayList<>();
+
+        requestTransacsList();
+
+
     }
 }
